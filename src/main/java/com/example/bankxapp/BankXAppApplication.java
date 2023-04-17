25d78e34d0b4ee25881c8e389f44f16de73347ae
unknown <tjkungwane@gmial.com>
2023-04-17 17:39:08 +0200

    my first commit

---
+++ src/main/java/com/example/bankxapp/BankXAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankXAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankXAppApplication.class, args);
+	}
+
+}
